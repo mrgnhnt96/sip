68a55e896ed9ee39737143f216dcd28d2625eba2
Morgan <mrgnhnt96@gmail.com>
2024-01-17 05:57:13 -0700

    feat: add tests for find

--- packages/script_runner/test/lib/config/scripts_config_test.dart
+++ packages/script_runner/test/lib/config/scripts_config_test.dart
@@ -5,112 +5,15 @@ import 'package:test/test.dart';
 
 void main() {
   group('$ScriptsConfig', () {
-    test('can parse empty config', () {
-      final config = ScriptsConfig.fromJson({});
-      expect(config.scripts, isEmpty);
-    });
-
-    test('can parse string command', () {
-      final config = ScriptsConfig.fromJson({
-        'test': 'echo "test"',
+    group('can parse', () {
+      test('can parse empty config', () {
+        final config = ScriptsConfig.fromJson({});
+        expect(config.scripts, isEmpty);
       });
 
-      expect(
-        config.scripts,
-        {
-          'test': const Script.defaults(
-            commands: ['echo "test"'],
-          ),
-        },
-      );
-    });
-
-    test('can parse list string command', () {
-      final config = ScriptsConfig.fromJson({
-        'test': [
-          'echo "test"',
-          'echo "test2"',
-        ],
-      });
-
-      expect(
-        config.scripts,
-        {
-          'test': const Script.defaults(
-            commands: [
-              'echo "test"',
-              'echo "test2"',
-            ],
-          ),
-        },
-      );
-    });
-
-    group('nested', () {
-      test('can parse string', () {
+      test('can parse string command', () {
         final config = ScriptsConfig.fromJson({
-          'test': {
-            'nested': 'echo "test"',
-          },
-        });
-
-        expect(
-          config.scripts,
-          {
-            'test': const Script.defaults(
-              scripts: ScriptsConfig(
-                scripts: {
-                  'nested': const Script.defaults(
-                    commands: ['echo "test"'],
-                  ),
-                },
-              ),
-            ),
-          },
-        );
-      });
-
-      test('can parse string and ignores other entries', () {
-        final config = ScriptsConfig.fromJson({
-          'test': {
-            Keys.scripts: 'echo "test"',
-            Keys.description: 'this is a test',
-            Keys.aliases: ['test'],
-            'test2': 'echo "test2"',
-          },
-        });
-
-        expect(config.scripts, hasLength(1));
-        expect(config.scripts.keys, ['test']);
-
-        final first = config.scripts.values.first;
-        expect(first.commands, ['echo "test"']);
-      });
-    });
-
-    group('using alternate key', () {
-      test('can parse empty script', () {
-        final config = ScriptsConfig.fromJson({
-          'test': {
-            Keys.scripts: null,
-          },
-        });
-
-        expect(
-          config.scripts,
-          {
-            'test': const Script.defaults(
-              commands: [],
-            ),
-          },
-        );
-      });
-
-      test('can parse string script', () {
-        final config = ScriptsConfig.fromJson({
-          'test': {
-            Keys.scripts: 'echo "test"',
-          },
+          'test': 'echo "test"',
         });
 
         expect(
@@ -123,14 +26,12 @@ void main() {
         );
       });
 
-      test('can parse list string script', () {
+      test('can parse list string command', () {
         final config = ScriptsConfig.fromJson({
-          'test': {
-            Keys.scripts: [
-              'echo "test"',
-              'echo "test2"',
-            ],
-          },
+          'test': [
+            'echo "test"',
+            'echo "test2"',
+          ],
         });
 
         expect(
@@ -145,6 +46,164 @@ void main() {
           },
         );
       });
+
+      group('nested', () {
+        test('can parse string', () {
+          final config = ScriptsConfig.fromJson({
+            'test': {
+              'nested': 'echo "test"',
+            },
+          });
+
+          expect(
+            config.scripts,
+            {
+              'test': Script.defaults(
+                scripts: ScriptsConfig(
+                  scripts: {
+                    'nested': const Script.defaults(
+                      commands: ['echo "test"'],
+                    ),
+                  },
+                ),
+              ),
+            },
+          );
+        });
+
+        test('can parse string and ignores other entries', () {
+          final config = ScriptsConfig.fromJson({
+            'test': {
+              Keys.scripts: 'echo "test"',
+              Keys.description: 'this is a test',
+              Keys.aliases: ['test'],
+              'test2': 'echo "test2"',
+            },
+          });
+
+          expect(config.scripts, hasLength(1));
+          expect(config.scripts.keys, ['test']);
+
+          final first = config.scripts.values.first;
+          expect(first.commands, ['echo "test"']);
+        });
+      });
+
+      group('using alternate key', () {
+        test('can parse empty script', () {
+          final config = ScriptsConfig.fromJson({
+            'test': {
+              Keys.scripts: null,
+            },
+          });
+
+          expect(
+            config.scripts,
+            {
+              'test': const Script.defaults(
+                commands: [],
+              ),
+            },
+          );
+        });
+
+        test('can parse string script', () {
+          final config = ScriptsConfig.fromJson({
+            'test': {
+              Keys.scripts: 'echo "test"',
+            },
+          });
+
+          expect(
+            config.scripts,
+            {
+              'test': const Script.defaults(
+                commands: ['echo "test"'],
+              ),
+            },
+          );
+        });
+
+        test('can parse list string script', () {
+          final config = ScriptsConfig.fromJson({
+            'test': {
+              Keys.scripts: [
+                'echo "test"',
+                'echo "test2"',
+              ],
+            },
+          });
+
+          expect(
+            config.scripts,
+            {
+              'test': const Script.defaults(
+                commands: [
+                  'echo "test"',
+                  'echo "test2"',
+                ],
+              ),
+            },
+          );
+        });
+      });
+    });
+
+    group('skips entry when', () {
+      test('key contains spaces', () {
+        final config = ScriptsConfig.fromJson({
+          'test test': 'echo "test"',
+        });
+
+        expect(config.scripts, isEmpty);
+      });
+    });
+  });
+
+  group('find script', () {
+    test('can find script by name or alias', () {
+      final commands = ['echo "banana"'];
+      final config = ScriptsConfig(scripts: {
+        'banana': Script.defaults(
+          aliases: {'b', 'ban'},
+          commands: commands,
+        ),
+      });
+
+      final b = config.find(['b']);
+      expect(b, isNotNull);
+      expect(b!.commands, commands);
+
+      final ban = config.find(['ban']);
+      expect(ban, isNotNull);
+      expect(ban!.commands, commands);
+
+      final banana = config.find(['banana']);
+      expect(banana, isNotNull);
+      expect(banana!.commands, commands);
+
+      final other = config.find(['other']);
+      expect(other, isNull);
+    });
+
+    test('finds nested script', () {
+      const commands = ['echo "patrick"'];
+      final config = ScriptsConfig(scripts: {
+        'bikini': Script.defaults(
+          scripts: ScriptsConfig(
+            scripts: {
+              'bottom': const Script.defaults(
+                commands: commands,
+              ),
+            },
+          ),
+        ),
+      });
+
+      final script = config.find(['bikini', 'bottom']);
+
+      expect(script, isNotNull);
+      expect(script!.commands, commands);
     });
   });
 }
