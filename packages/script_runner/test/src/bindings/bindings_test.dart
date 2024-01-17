8d63c1aab69fd67d9559124f29ef923fdb9afb10
Morgan <mrgnhnt96@gmail.com>
2024-01-17 11:10:29 -0700

    feat: create interface for bindings, add tests

--- packages/script_runner/test/src/bindings/bindings_test.dart
+++ packages/script_runner/test/src/bindings/bindings_test.dart
@@ -1,10 +1,10 @@
-import 'package:sip_script_runner/src/bindings/bindings.dart';
+import 'package:sip_script_runner/src/bindings/bindings_impl.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  final bindings = const Bindings();
+  final bindings = const BindingsImpl();
 
-  group('$Bindings', () {
+  group('$BindingsImpl', () {
     test('blobFileName', () {
       expect(bindings.blobFileName, isNotEmpty);
     });
