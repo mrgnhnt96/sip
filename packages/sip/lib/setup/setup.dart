b11bf541cf8fe2f2f3302ba781d776f790f0b616
Morgan <mrgnhnt96@gmail.com>
2024-01-19 09:26:15 -0700

    feat: add sip console to sip

--- packages/sip/lib/setup/setup.dart
+++ packages/sip/lib/setup/setup.dart
@@ -1,5 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:sip_console/sip_console.dart' hide setup;
+import 'package:sip_console/sip_console.dart' as console show setup;
 
 late GetIt getIt;
 
-void setupDependencyInjection() => getIt = GetIt.asNewInstance();
+void setup() {
+  getIt = GetIt.asNewInstance();
+
+  console.setup(getIt);
+
+  getIt.registerLazySingleton<SipConsole>(SipConsole.new);
+}
