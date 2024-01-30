838b8cc6c5d7e4b9eafe5021fa61c3fa88d9c5b6
Morgan <mrgnhnt96@gmail.com>
2024-01-29 17:10:36 -0700

    feat: list out files after compile

--- packages/script_runner/scripts/compile_blob.sh
+++ packages/script_runner/scripts/compile_blob.sh
@@ -97,6 +97,9 @@ cd "$NATIVE_DIR" || exit 1
 cargo build --release
 RELEASE=$(join "$NATIVE_DIR" "target" "release" "libsip_script_runner.$EXT")
 
+# list out files found int the release directory
+ls -l "$(join "$NATIVE_DIR", "target", "release")"
+
 cd "$SCRIPTS_RUNNER_DIR" || exit 1
 BLOBS_DIR=$(join "$SCRIPTS_RUNNER_DIR" "lib" "src" "blobs")
 BLOB_FILE="""$PLATFORM""_""$ARCH"".$EXT"
