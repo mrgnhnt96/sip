e3e4a3cff83c69d7cc1f05b9e85baa300a9508cc
Morgan <mrgnhnt96@gmail.com>
2024-02-09 13:55:10 -0700

    feat: create `RunScriptHelper` mixin to sync runner commands utilities

---
+++ packages/sip/lib/utils/run_script_helper.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+
+import 'package:file/file.dart';
+import 'package:path/path.dart' as path;
+import 'package:args/command_runner.dart';
+import 'package:sip_cli/commands/list_command.dart';
+import 'package:sip_cli/domain/command_to_run.dart';
+import 'package:sip_cli/setup/setup.dart';
+import 'package:sip_cli/utils/exit_code.dart';
+import 'package:sip_console/sip_console.dart';
+import 'package:sip_console/utils/ansi.dart';
+import 'package:sip_script_runner/domain/optional_flags.dart';
+import 'package:sip_script_runner/sip_script_runner.dart';
+
+mixin RunScriptHelper on Command<ExitCode> {
+  ScriptsYaml get scriptsYaml;
+  Variables get variables;
+
+  String? _directory;
+  String _findDirectory() {
+    final nearest = scriptsYaml.nearest();
+    final directory = nearest == null
+        ? getIt<FileSystem>().currentDirectory.path
+        : path.dirname(nearest);
+
+    return directory;
+  }
+
+  String get directory => _directory ??= _findDirectory();
+
+  void addFlags() {
+    argParser.addFlag(
+      'list',
+      abbr: 'l',
+      negatable: false,
+      aliases: ['ls', 'h'],
+      help: 'List all available scripts',
+    );
+  }
+
+  FutureOr<ExitCode?> validate(List<String>? keys) async {
+    if (keys == null || keys.isEmpty) {
+      const warning = 'No script specified, choose from:';
+      getIt<SipConsole>()
+        ..w(lightYellow.wrap(warning) ?? warning)
+        ..emptyLine();
+
+      return ListCommand(
+        scriptsYaml: scriptsYaml,
+      ).run();
+    }
+
+    if (keys.any((e) => e.startsWith('_'))) {
+      getIt<SipConsole>().e(
+        r'''Private scripts are not intended to be invoked, only to be used as a references in other scripts.
+
+```yaml
+format:
+  _: dart format .
+  ui: cd packages/ui && {$format:_}
+```
+
+$ sip format ui
+''',
+      );
+      return ExitCode.config;
+    }
+
+    return null;
+  }
+
+  OptionalFlags optionalFlags(List<String> keys) {
+    final flagStartAt = keys.indexWhere((e) => e.startsWith('-'));
+    final flagArgs = flagStartAt == -1 ? <String>[] : keys.sublist(flagStartAt);
+
+    return OptionalFlags(flagArgs);
+  }
+
+  void _listOutScript(Script script) {
+    getIt<SipConsole>()
+      ..emptyLine()
+      ..l(script.name)
+      ..print(script.listOut(
+        wrapKey: (s) => lightGreen.wrap(s) ?? s,
+        wrapMeta: (s) => lightBlue.wrap(s) ?? s,
+      ))
+      ..emptyLine();
+  }
+
+  (ExitCode?, List<String>? commands) getCommands(List<String> keys) {
+    final flagStartAt = keys.indexWhere((e) => e.startsWith('-'));
+    final scriptKeys = keys.sublist(0, flagStartAt == -1 ? null : flagStartAt);
+
+    final content = scriptsYaml.parse();
+    if (content == null) {
+      getIt<SipConsole>().e('No ${ScriptsYaml.fileName} file found');
+      return (ExitCode.noInput, null);
+    }
+
+    final scriptConfig = ScriptsConfig.fromJson(content);
+
+    final script = scriptConfig.find(scriptKeys);
+
+    if (script == null) {
+      getIt<SipConsole>().e('No script found for ${scriptKeys.join(' ')}');
+      return (ExitCode.config, null);
+    }
+
+    if (script.commands.isEmpty) {
+      getIt<SipConsole>()
+        ..w('There are no commands to run for "${scriptKeys.join(' ')}"')
+        ..w('Here are the available scripts:');
+
+      _listOutScript(script);
+
+      return (ExitCode.config, null);
+    }
+
+    if (argResults?.wasParsed('list') ?? false) {
+      _listOutScript(script);
+
+      return (ExitCode.success, null);
+    }
+
+    final resolvedCommands = variables.replace(
+      script,
+      scriptConfig,
+      flags: optionalFlags(keys),
+    );
+
+    return (null, resolvedCommands);
+  }
+
+  Iterable<CommandToRun> _commandsToRun(List<String> commands) sync* {
+    for (final command in commands) {
+      yield CommandToRun(
+        command: command,
+        label: command,
+        workingDirectory: directory,
+      );
+    }
+  }
+
+  (ExitCode?, Iterable<CommandToRun>?) commandsToRun(List<String> keys) {
+    final (exitCode, commands) = getCommands(keys);
+
+    if (exitCode != null) {
+      return (exitCode, null);
+    }
+
+    assert(commands != null, 'commands should not be null');
+    commands!;
+
+    return (null, _commandsToRun(commands));
+  }
+}
---
+++ packages/sip/test/lib/utils/run_script_helper_test.dart
@@ -0,0 +1,226 @@
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'package:sip_cli/domain/command_to_run.dart';
+import 'package:sip_cli/utils/exit_code.dart';
+import 'package:sip_cli/utils/run_script_helper.dart';
+import 'package:sip_script_runner/domain/optional_flags.dart';
+import 'package:sip_script_runner/sip_script_runner.dart';
+import 'package:test/test.dart';
+
+import '../../utils/setup_testing_dependency_injection.dart';
+
+class _FakeCommand extends Command<ExitCode> with RunScriptHelper {
+  _FakeCommand({
+    required this.scriptsYaml,
+    required this.variables,
+  }) {
+    addFlags();
+  }
+
+  ArgResults? argResults;
+
+  @override
+  final ScriptsYaml scriptsYaml;
+
+  @override
+  final Variables variables;
+
+  @override
+  String get description => '';
+
+  @override
+  String get name => '';
+}
+
+class _FakeScriptsYaml implements ScriptsYaml {
+  String? nearestFile;
+  @override
+  String? nearest() {
+    if (nearestFile != null) {
+      return nearestFile;
+    }
+
+    throw UnimplementedError();
+  }
+
+  Map<String, dynamic>? content;
+
+  @override
+  Map<String, dynamic>? parse() => content ?? {};
+
+  @override
+  String? retrieveContent() {
+    throw UnimplementedError();
+  }
+}
+
+class _FakeVariables implements Variables {
+  @override
+  CWD get cwd => throw UnimplementedError();
+
+  @override
+  Map<String, String?> populate() {
+    throw UnimplementedError();
+  }
+
+  @override
+  PubspecYaml get pubspecYaml => throw UnimplementedError();
+
+  @override
+  List<String> replace(
+    Script script,
+    ScriptsConfig config, {
+    OptionalFlags? flags,
+  }) {
+    return script.commands;
+  }
+
+  @override
+  ScriptsYaml get scriptsYaml => throw UnimplementedError();
+}
+
+void main() {
+  late _FakeCommand command;
+
+  setUp(() {
+    setupTestingDependencyInjection();
+
+    command = _FakeCommand(
+      scriptsYaml: _FakeScriptsYaml(),
+      variables: _FakeVariables(),
+    );
+  });
+
+  group('$RunScriptHelper', () {
+    group('#directory', () {
+      test('should return the current directory if no scripts.yaml is found',
+          () {
+        expect(command.directory, '');
+      });
+
+      test('should return the directory of the nearest scripts.yaml', () {
+        (command.scriptsYaml as _FakeScriptsYaml).nearestFile =
+            'some/path/to/test/scripts.yaml';
+
+        expect(command.directory, 'some/path/to/test');
+      });
+    });
+
+    group('#addFlags', () {
+      test('should add the list flag', () {
+        command.addFlags();
+        expect(command.argParser.options, contains('list'));
+      });
+    });
+
+    group('#validate', () {
+      test('returns an exit code when no keys are provided', () async {
+        final result = await command.validate(null);
+        expect(result, isA<ExitCode>());
+      });
+
+      test('returns an exit code when a private script is provided', () async {
+        final result = await command.validate(['_private']);
+        expect(result, isA<ExitCode>());
+
+        final result2 = await command.validate(['_private', 'public']);
+        expect(result2, isA<ExitCode>());
+
+        final result3 = await command.validate(['public', '_private']);
+        expect(result3, isA<ExitCode>());
+      });
+    });
+
+    group('#optionalFlags', () {
+      test('should return an nothing', () {
+        expect(command.optionalFlags([]), OptionalFlags([]));
+      });
+
+      test('should return a map with the provided flags', () {
+        expect(
+          command.optionalFlags(['--verbose', 'true']),
+          OptionalFlags(['--verbose', 'true']),
+        );
+
+        expect(
+          command.optionalFlags(['some', 'script', '--verbose', 'true']),
+          OptionalFlags(['--verbose', 'true']),
+        );
+      });
+    });
+
+    group('#getCommands', () {
+      test('should return the list of commands', () {
+        (command.scriptsYaml as _FakeScriptsYaml).content = {
+          'pub': 'echo "pub"',
+        };
+
+        final (exitCode, commands) = command.getCommands(['pub']);
+        expect(exitCode, isNull);
+        expect(commands, ['echo "pub"']);
+      });
+
+      test('should return an exit code when the script is not found', () {
+        final (exitCode, commands) = command.getCommands(['pub']);
+        expect(exitCode, isA<ExitCode>());
+        expect(commands, isNull);
+      });
+
+      test('should return an exit code when the script is empty', () {
+        (command.scriptsYaml as _FakeScriptsYaml).content = {
+          'pub': null,
+        };
+
+        final (exitCode, commands) = command.getCommands(['pub']);
+        expect(exitCode, isA<ExitCode>());
+        expect(commands, isNull);
+      });
+
+      test('should return an exit code with list option is provided', () {
+        command.argResults = command.argParser.parse(['--list']);
+
+        (command.scriptsYaml as _FakeScriptsYaml).content = {
+          'pub': 'echo "pub"',
+        };
+
+        final (exitCode, commands) = command.getCommands(['pub']);
+
+        expect(exitCode, isA<ExitCode>());
+        expect(commands, isNull);
+      });
+    });
+
+    group('#commandsToRun', () {
+      test('should return the list of commands', () {
+        (command.scriptsYaml as _FakeScriptsYaml).nearestFile =
+            'some/path/to/test/scripts.yaml';
+
+        (command.scriptsYaml as _FakeScriptsYaml).content = {
+          'pub': 'echo "pub"',
+        };
+
+        final (exitCode, commands) = command.commandsToRun(['pub']);
+        expect(exitCode, isNull);
+
+        expect(commands, isNotNull);
+
+        final commandToRun = commands!.first;
+        final expected = CommandToRun(
+          command: 'echo "pub"',
+          label: 'echo "pub"',
+          workingDirectory: 'some/path/to/test',
+        );
+
+        expect(commandToRun.command, expected.command);
+        expect(commandToRun.label, expected.label);
+        expect(commandToRun.workingDirectory, expected.workingDirectory);
+      });
+
+      test('should return an exit code when the script is not found', () {
+        final (exitCode, commands) = command.commandsToRun(['pub']);
+        expect(exitCode, isA<ExitCode>());
+        expect(commands, isNull);
+      });
+    });
+  });
+}
