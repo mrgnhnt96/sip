4d720665a38b654f818abb8e651eae37401ba5f3
Morgan <mrgnhnt96@gmail.com>
2024-03-15 14:11:42 -0700

    feat: support ignoring lockfile exit

--- packages/sip/lib/commands/pub_get_command.dart
+++ packages/sip/lib/commands/pub_get_command.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: cascade_invocations
 
+import 'package:mason_logger/mason_logger.dart' hide ExitCode;
 import 'package:sip_cli/commands/a_pub_command.dart';
+import 'package:sip_cli/utils/exit_code.dart';
 
 /// The `pub get` command.
 class PubGetCommand extends APubCommand {
@@ -12,29 +14,33 @@ class PubGetCommand extends APubCommand {
     required super.bindings,
     required super.findFile,
   }) {
-    argParser.addFlag(
-      'offline',
-      help: 'Use cached packages instead of accessing the network.',
-    );
-
-    argParser.addFlag(
-      'dry-run',
-      abbr: 'n',
-      negatable: false,
-      help: "Report what dependencies would change but don't change any.",
-    );
-
-    argParser.addFlag(
-      'enforce-lockfile',
-      negatable: false,
-      help: 'Enforce pubspec.lock. Fail resolution if '
-          'pubspec.lock does not satisfy pubspec.yaml',
-    );
-
-    argParser.addFlag(
-      'precompile',
-      help: 'Build executables in immediate dependencies.',
-    );
+    argParser
+      ..addFlag(
+        'offline',
+        help: 'Use cached packages instead of accessing the network.',
+      )
+      ..addFlag(
+        'dry-run',
+        abbr: 'n',
+        negatable: false,
+        help: "Report what dependencies would change but don't change any.",
+      )
+      ..addFlag(
+        'enforce-lockfile',
+        negatable: false,
+        help: 'Enforce pubspec.lock. Fail resolution if '
+            'pubspec.lock does not satisfy pubspec.yaml',
+      )
+      ..addFlag(
+        'precompile',
+        help: 'Build executables in immediate dependencies.',
+      )
+      ..addFlag(
+        'ignore-lockfile-exit',
+        negatable: false,
+        help: 'When the lockfile is out of date and `--enforce-lockfile` '
+            'is used, treat the exit code as success.',
+      );
   }
 
   @override
@@ -47,4 +53,28 @@ class PubGetCommand extends APubCommand {
         if (argResults!['enforce-lockfile'] as bool) '--enforce-lockfile',
         if (argResults!['precompile'] as bool) '--precompile',
       ];
+
+  @override
+  ExitCode onFinish(ExitCode exitCode) {
+    final ignoreLockfileExitCode = argResults!['ignore-lockfile-exit'] as bool;
+
+    if (!ignoreLockfileExitCode) {
+      return exitCode;
+    }
+
+    if (exitCode.code == 65) {
+      logger.write('\n');
+      logger.detail('Ignoring exit code 65');
+
+      logger.warn('The lockfile is out of date');
+      logger.info(
+        '${green.wrap('Successfully')} ${darkGray.wrap('got dependencies.')}',
+      );
+
+      logger.write('\n');
+      return ExitCode.success;
+    }
+
+    return exitCode;
+  }
 }
