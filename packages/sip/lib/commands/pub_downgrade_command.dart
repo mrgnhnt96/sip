89c1c140f8e6df3864b4bcadb34563cbda462790
Morgan <mrgnhnt96@gmail.com>
2024-11-20 09:52:32 -0700

    feat: create downgrade command

---
+++ packages/sip/lib/commands/pub_downgrade_command.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: cascade_invocations
+
+import 'package:sip_cli/commands/a_pub_command.dart';
+
+/// The `pub downgrade` command.
+///
+/// https://github.com/dart-lang/pub/blob/master/lib/src/command/downgrade.dart
+class PubDowngradeCommand extends APubCommand {
+  PubDowngradeCommand({
+    required super.pubspecLock,
+    required super.pubspecYaml,
+    required super.fs,
+    required super.logger,
+    required super.bindings,
+    required super.findFile,
+  }) {
+    argParser.addFlag(
+      'offline',
+      help: 'Use cached packages instead of accessing the network.',
+    );
+
+    argParser.addFlag(
+      'dry-run',
+      abbr: 'n',
+      negatable: false,
+      help: "Report what dependencies would change but don't change any.",
+    );
+
+    argParser.addFlag(
+      'tighten',
+      help:
+          'Updates lower bounds in pubspec.yaml to match the resolved version.',
+      negatable: false,
+    );
+  }
+
+  @override
+  String get name => 'downgrade';
+
+  @override
+  ({Duration? dart, Duration? flutter}) get retryAfter => (
+        dart: const Duration(milliseconds: 750),
+        flutter: const Duration(milliseconds: 4000)
+      );
+
+  @override
+  List<String> get pubFlags => [
+        if (argResults!['offline'] as bool) '--offline',
+        if (argResults!['dry-run'] as bool) '--dry-run',
+        if (argResults!['tighten'] as bool) '--tighten',
+      ];
+}
