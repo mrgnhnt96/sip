63c8659aafd9aea8110d5e1271ed54ba6298ff29
Morgan <mrgnhnt96@gmail.com>
2024-11-20 09:45:06 -0700

    refactor: use dart or flutter mixin

--- packages/sip/lib/commands/a_pub_command.dart
+++ packages/sip/lib/commands/a_pub_command.dart
@@ -13,12 +13,12 @@ import 'package:sip_cli/domain/pubspec_lock.dart';
 import 'package:sip_cli/domain/pubspec_yaml.dart';
 import 'package:sip_cli/domain/run_many_scripts.dart';
 import 'package:sip_cli/domain/run_one_script.dart';
-import 'package:sip_cli/utils/determine_flutter_or_dart.dart';
+import 'package:sip_cli/utils/dart_or_flutter_mixin.dart';
 import 'package:sip_cli/utils/exit_code.dart';
 import 'package:sip_cli/utils/exit_code_extensions.dart';
 
 /// A command that runs `pub *`.
-abstract class APubCommand extends Command<ExitCode> {
+abstract class APubCommand extends Command<ExitCode> with DartOrFlutterMixin {
   APubCommand({
     required this.pubspecLock,
     required this.pubspecYaml,
@@ -66,32 +66,21 @@ abstract class APubCommand extends Command<ExitCode> {
 
   List<String> get pubFlags => [];
 
+  @override
   final PubspecLock pubspecLock;
   final PubspecYaml pubspecYaml;
   final Bindings bindings;
+  @override
   final FindFile findFile;
+  @override
   final Logger logger;
+  @override
   final FileSystem fs;
 
   ({Duration? dart, Duration? flutter})? get retryAfter => null;
 
   @override
   String get description => '$name dependencies for pubspec.yaml files';
-
-  void warnDartOrFlutter({
-    required bool isFlutterOnly,
-    required bool isDartOnly,
-  }) {
-    if (isDartOnly || isFlutterOnly) {
-      if (isDartOnly && !isFlutterOnly) {
-        logger.info('Running only in dart packages');
-      } else if (isFlutterOnly && !isDartOnly) {
-        logger.info('Running only in flutter packages');
-      } else {
-        logger.info('Running both dart and flutter');
-      }
-    }
-  }
 
   ExitCode onFinish(ExitCode exitCode) => exitCode;
 
@@ -124,61 +113,40 @@ abstract class APubCommand extends Command<ExitCode> {
       return ExitCode.unavailable;
     }
 
-    final commands = (
-      dart: <CommandToRun>[],
-      flutter: <CommandToRun>[],
-      ordered: <(DetermineFlutterOrDart, CommandToRun)>[],
+    final commands = resolveFlutterAndDart(
+      pubspecs,
+      dartOnly: dartOnly,
+      flutterOnly: flutterOnly,
+      (flutterOrDart) {
+        final project = path.dirname(flutterOrDart.pubspecYaml);
+
+        final relativeDir = path.relative(
+          project,
+          from: fs.currentDirectory.path,
+        );
+
+        final tool = flutterOrDart.tool();
+
+        final padding = max('flutter'.length, tool.length) - tool.length;
+        var toolString = '(${cyan.wrap(tool)})';
+        toolString = darkGray.wrap(toolString) ?? toolString;
+        toolString = toolString.padRight(padding + toolString.length);
+
+        var pathString = './$relativeDir';
+        pathString = lightYellow.wrap(pathString) ?? pathString;
+
+        final label = '$toolString $pathString';
+
+        final command = CommandToRun(
+          command: '$tool pub $name ${pubFlags.join(' ')}',
+          workingDirectory: project,
+          keys: ['dart', 'pub', name, ...pubFlags],
+          label: label,
+        );
+
+        return command;
+      },
     );
-    for (final pubspec in pubspecs) {
-      final flutterOrDart = DetermineFlutterOrDart(
-        pubspecYaml: pubspec,
-        pubspecLock: pubspecLock,
-        findFile: findFile,
-      );
-
-      final project = path.dirname(pubspec);
-
-      final relativeDir = path.relative(
-        project,
-        from: fs.currentDirectory.path,
-      );
-
-      if (dartOnly ^ flutterOnly) {
-        if (dartOnly && flutterOrDart.isFlutter) {
-          logger.detail('Skipping flutter project: $relativeDir');
-          continue;
-        } else if (flutterOnly && flutterOrDart.isDart) {
-          logger.detail('Skipping dart project: $relativeDir');
-          continue;
-        }
-      }
-
-      final tool = flutterOrDart.tool();
-
-      final padding = max('flutter'.length, tool.length) - tool.length;
-      var toolString = '(${cyan.wrap(tool)})';
-      toolString = darkGray.wrap(toolString) ?? toolString;
-      toolString = toolString.padRight(padding + toolString.length);
-
-      var pathString = './$relativeDir';
-      pathString = lightYellow.wrap(pathString) ?? pathString;
-
-      final label = '$toolString $pathString';
-
-      final command = CommandToRun(
-        command: '$tool pub $name ${pubFlags.join(' ')}',
-        workingDirectory: project,
-        keys: ['dart', 'pub', name, ...pubFlags],
-        label: label,
-      );
-
-      commands.ordered.add((flutterOrDart, command));
-      if (flutterOrDart.isFlutter) {
-        commands.flutter.add(command);
-      } else {
-        commands.dart.add(command);
-      }
-    }
 
     if (concurrent) {
       final runners = [
