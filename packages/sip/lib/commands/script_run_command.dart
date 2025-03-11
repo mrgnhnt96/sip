b168a3696a8f776611d67c9c254294e07fd09190
Morgan <mrgnhnt96@gmail.com>
2025-03-11 15:57:45 -0700

    fix: change from sequentially true to false

--- packages/sip/lib/commands/script_run_command.dart
+++ packages/sip/lib/commands/script_run_command.dart
@@ -249,7 +249,7 @@ class ScriptRunCommand extends Command<ExitCode>
       final exitCodes = await runManyScripts.run(
         commands: concurrentRuns.toList(),
         bail: bail,
-        sequentially: true,
+        sequentially: false,
       );
 
       exitCodes.printErrors(concurrentRuns, logger);
