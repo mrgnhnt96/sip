ba58f11283802eb9f4940a324123f847913d6bf8
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2025-12-16 09:29:46 -0700

    refactor: delete lint kit files

--- sip_lint_kit/lib/entry_point.dart
+++
@@ -1,6 +0,0 @@
-import 'package:lint_kit/lint_kit.dart';
-import 'package:sip_lint_kit/src/sip_lint_kit_analysis.dart';
-
-LintKitAnalyzer entrypoint() {
-  return const SipLintKitAnalysis();
-}
--- sip_lint_kit/lib/src/lints/utils/linter.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-import 'package:lint_kit/lint_kit.dart';
-
-abstract interface class Linter {
-  const Linter();
-
-  FutureOr<Iterable<Lint>> lint(AnalyzedFile file);
-}
